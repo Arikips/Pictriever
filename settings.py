b5c516531e9e66355eeb13cc92535722bf81e03c
Arikips <116493573+Arikips@users.noreply.github.com>
2024-09-24 03:43:07 +0200

    Add files via upload

--- settings.py
+++ settings.py
@@ -11,13 +11,29 @@ class Settings:
             self.hotkeys = {
                 'select_area': 'ctrl+alt+s',
                 'scan_area': 'ctrl+alt+x',
-                'terminate_app': 'ctrl+alt+q'
+                'terminate_app': 'ctrl+alt+q',
+                'toggle_auto_scan': 'ctrl+alt+a'
             }
+            self.auto_scan_interval = 2
             self.save_settings()
         else:
             with open(self.config_file, 'r') as f:
-                self.hotkeys = json.load(f)
+                data = json.load(f)
+                self.hotkeys = {
+                    'select_area': data.get('select_area', 'ctrl+alt+s'),
+                    'scan_area': data.get('scan_area', 'ctrl+alt+x'),
+                    'terminate_app': data.get('terminate_app', 'ctrl+alt+q'),
+                    'toggle_auto_scan': data.get('toggle_auto_scan', 'ctrl+alt+a')
+                }
+                self.auto_scan_interval = data.get('auto_scan_interval', 10)
 
     def save_settings(self):
+        data = {
+            'select_area': self.hotkeys['select_area'],
+            'scan_area': self.hotkeys['scan_area'],
+            'terminate_app': self.hotkeys['terminate_app'],
+            'toggle_auto_scan': self.hotkeys['toggle_auto_scan'],
+            'auto_scan_interval': self.auto_scan_interval
+        }
         with open(self.config_file, 'w') as f:
-            json.dump(self.hotkeys, f, indent=4)
+            json.dump(data, f, indent=4)
